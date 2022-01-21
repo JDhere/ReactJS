503eddc73be0d4efb027e802423b57b5705d3190
JDhere <thejasonjerry@gmail.com>
2022-01-21 13:17:11 +0530

    Day 4 Final

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import './card.styles.css';
 
 export const Card = (props) => (
     <div className="card-container">
-        <img alt="monster" src={`https://robohash.org/${props.avenger.id}?set=set2&size=180x180`}></img>
+        <img alt="monster" src={`https://robohash.org/${props.avenger.id}?set=set6&size=180x180`}></img>
         <h1>{ props.avenger.name }</h1>
     </div>
 )
