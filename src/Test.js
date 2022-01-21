d9dc13c41e7d072f782c6a307abb016ad49de7b4
tychons <tychons@server.example.com>
2022-01-21 12:12:49 +0530

    Day 4

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from "./components/search-box/search-box.component";
 
 /** Using Class we have made this one */
 class Test extends Component {
@@ -14,6 +15,10 @@ class Test extends Component {
 
     }
 
+    handleChange = e => {
+        this.setState({ searchField: e.target.value })
+     }
+
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users/')
         .then(response => response.json())
@@ -25,7 +30,11 @@ class Test extends Component {
         const filteredAvengers = avengers.filter(avenger => avenger.name.toLowerCase().includes(searchField.toLowerCase()))
         return (
             <div className="App">
-                <input type="search" placeholder="Search Avengers" onChange={e => this.setState({ searchField: e.target.value})}></input>
+                {/* <input type="search" placeholder="Search Avengers" onChange={e => this.setState({ searchField: e.target.value})}></input> */}
+                <SearchBox
+                    placeholder='search here'
+                    handleChange={this.handleChange}
+                />
                 <CardList avengers={filteredAvengers}>
                 </CardList>
             </div>
